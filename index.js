aaa184eb1e4d57537d77b483f1f6cf3d83903734
qiuxiang <997415019@qq.com>
2020-05-13 15:38:06 +0800

    1.1

---
+++ index.js
@@ -0,0 +1,10 @@
+$('.page .content').hide();
+$('.page .contents .content').eq(0).show();
+$('.page .head').eq(0).addClass('active');
+
+$('.headerTab .head').click(function () {
+    $('.headerTab .head').removeClass('active');
+    $(this).addClass('active');
+    var index = $(this).index();
+    $('.page .content').hide().eq(index).show();
+})
